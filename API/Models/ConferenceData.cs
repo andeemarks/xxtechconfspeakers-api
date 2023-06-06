47b8bfe7ac691d759d625c54b01438a67ce6af68
Andy Marks <vampwillow@gmail.com>
2023-06-06 19:51:30 +1000

    Created backing model for single conf speaker summary

--- API/Models/ConferenceData.cs
+++ API/Models/ConferenceData.cs
@@ -9,9 +9,28 @@ namespace API.Models
 
         }
 
-        public static IEnumerable SpeakerSummary()
+        public static List<SpeakerSummary> SpeakerSummary()
         {
-            return new List<string>() { "Hello, World!" };
+            return new List<SpeakerSummary>() { new SpeakerSummary("Hello", "Here", "reddit") };
         }
+    }
+
+    public class SpeakerSummary
+    {
+        public SpeakerSummary(string name, string location, string source)
+        {
+            Name = name;
+            Location = location;
+            Source = source;
+        }
+
+        public string Name { init; get; }
+        public string Location { get; init; }
+        public int Year { get; init;  }
+        public int TotalSpeakers { get; init; }
+        public int NumberOfWomen { get; init; }
+        public string Source { init; get; }
+        public DateOnly DateAdded { get; init; }
+        public DateOnly ConfDate { get; init; }
     }
 }
--- UnitTests/ConferenceDataTests.cs
+++ UnitTests/ConferenceDataTests.cs
@@ -8,7 +8,15 @@ public class ConferenceDataModelTests
     public void Test_AllConferenceDataIsLoaded()
     {
         var conferenceData = ConferenceData.SpeakerSummary();
-        Assert.NotNull(conferenceData);
-        Assert.NotEmpty(conferenceData);
+        
+        var speakerSummary = conferenceData[0];
+        Assert.NotNull(speakerSummary.Name);
+        Assert.NotNull(speakerSummary.Location);
+        Assert.True(speakerSummary.Year >= 2015);
+        Assert.True(speakerSummary.TotalSpeakers > 0);
+        Assert.True(speakerSummary.NumberOfWomen >= 0);
+        Assert.NotNull(speakerSummary.Source);
+        Assert.NotNull(speakerSummary.DateAdded);
+        Assert.NotNull(speakerSummary.ConfDate);
     }
 }
